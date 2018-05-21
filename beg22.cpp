7427d961f86caf4ada6f6196bad8a815375fc716
HariniRamasamy7 <36732343+HariniRamasamy7@users.noreply.github.com>
2018-05-21 11:51:32 +0530

    beg22.cpp

---
+++ beg22.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{ int a[100];
+ int n=5,i,large;
+ cout<<"enter the elements";
+ for(i=0;i<=n;i++)
+ { 
+   cin>>a[i];
+   large=a[i];
+   if(large>=n)
+   { cout<<"the largest number is"<<a[i];
+   	
+   }
+ }
+   
+ }
